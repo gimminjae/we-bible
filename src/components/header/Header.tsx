c6635e93b3036407f339c777b942e5e467d78987
gimminjae <min356812@naver.com>
2025-05-23 15:52:34 +0900

    project init

--- src/components/header/Header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <header className="bg-base-100">
-      <div className="container mx-auto">
-        <h1>Header</h1>
-      </div>
-    </header>
-  )
-}
