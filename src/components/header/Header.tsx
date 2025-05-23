cccca3e1016065aa12b977a8bfe116dec27f706f
gimminjae <min356812@naver.com>
2025-05-23 15:27:01 +0900

    re init

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="bg-base-100">
+      <div className="container mx-auto">
+        <h1>Header</h1>
+      </div>
+    </header>
+  )
+}
